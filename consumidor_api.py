3ff01db1f0e34c8af49253731eeeec84e67d545a
Genilson Campos <genilson.medeiros07@gmail.com>
2021-10-04 11:31:45 -0300

    melhorando a performance

--- consumidor_api.py
+++ consumidor_api.py
@@ -12,10 +12,8 @@ response_json = response.json()
 
 for list_email in response_json:
     regex_tld = (re.findall(r'[\w\.-]+@[\w\.-]+\.org', list_email['email']))
-    regex_tld = filter(None, regex_tld)      
-    
-    for i in regex_tld:
-        print(i)
+    if len(regex_tld) != 0:
+        print(regex_tld) 
 
 
 
